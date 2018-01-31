538228b7cf7ace7b28c61bde2ecac7b3299a272f
zhaoquanjun <zhaoquanjun0@qq.com>
2018-01-31 17:09:22 +0800

    update connect me

--- js/resume.js
+++ js/resume.js
@@ -2,28 +2,21 @@
 
 //->音频的自动播放
 ~function () {
-    var audioBox = document.querySelector(".audio"),
-        myAudio = audioBox.getElementsByTagName("audio")[0];
-
-    //->延时播放音频文件,先让其他的内容加载
-    window.setTimeout(function () {
-        myAudio.play();
-        myAudio.addEventListener("canplay", function () {
-            //->当音频可以播放的时候触发这个事件
-            audioBox.style.display = "block";
-            audioBox.className += " audioMove";
+    
+    audioAutoPlay('audioPlayer')
+    function audioAutoPlay(id){
+        var audioPlayer = document.getElementById(id);
+        var play = function(){
+            audioPlayer.play();
+            document.removeEventListener("touchstart",play, false);
+        };
+        audioPlayer.play();
+        document.addEventListener("WeixinJSBridgeReady", function () {
+            play();
         }, false);
-    }, 1000);
-
-    //->点击音乐图标,实现音频的暂停或者播放
-    audioBox.addEventListener("click", function () {
-        if (myAudio.paused) {//->当前是暂停的,我让其播放
-            myAudio.play();
-            audioBox.className = "audio audioMove";
-            return;
-        }
-        //->当前是播放的,我让其暂停
-        myAudio.pause();
-        audioBox.className = "audio";
-    }, false);
+        document.addEventListener('YixinJSBridgeReady', function() {
+            play();
+        }, false);
+        document.addEventListener("touchstart",play, false);
+    }
 }();
